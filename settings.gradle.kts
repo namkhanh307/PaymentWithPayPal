a3dd6e5a731867e08f90cf7fa8f283262f1e5f2a
Nguyen Viet Nam Khanh <nguyenvietnamkhanhbl@gmail.com>
2025-02-25 11:20:16 +0700

    Update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,10 @@ dependencyResolutionManagement {
         mavenCentral()
         maven {
             url = uri("https://cardinalcommerceprod.jfrog.io/artifactory/android")
+            credentials {
+                username = "paypal_sgerritz"
+                password = "AKCp8jQ8tAahqpT5JjZ4FRP2mW7GMoFZ674kGqHmupTesKeAY2G8NcmPKLuTxTGkKjDLRzDUQ"
+            }
         }
     }
 }
